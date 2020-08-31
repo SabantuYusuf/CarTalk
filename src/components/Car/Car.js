fc763a5a9332cae3a632b543efe8a8a1e130e913
Sabantu <Sabantuyusuf@gmail.com>
2020-08-30 19:50:56 -0500

    repo check

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -26,9 +26,9 @@ function Car (props) {
             </div>
             {!list && (
                 <div className='car-card-action'>
-                    <h6><strong>Year</strong>{car.year}</h6>
-                    <h6><strong>Model</strong>{car.model}</h6>
-                    <h6><strong>Horsepower</strong>{car.horsepower}</h6>
+                    <h6><strong>Year </strong>{car.year}</h6>
+                    <h6><strong>Model </strong>{car.model}</h6>
+                    <h6><strong>Horsepower </strong>{car.horsepower}</h6>
                     <Like />
                     <Link to={`/cars/${car._id}/edit`} ><button className="edit">Edit</button></Link>
                     <button className='delete' onClick={handleDelete}>Delete</button>
